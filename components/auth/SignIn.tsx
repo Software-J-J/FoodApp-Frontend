3d5a6c2724e64990a5c312949a8b98712e8353f1
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-10-02 16:28:10 -0300

    added: new custom loading button witch can be provided with a pending status and a children prop to be used flexibly

--- components/auth/SignIn.tsx
+++ components/auth/SignIn.tsx
@@ -1,6 +1,13 @@
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+'use client'
+
+import { useState } from 'react'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import ErrorMessage from '@/components/shared/error-message'
+import { Form } from '@/components/ui/form'
 
 export default function SignIn() {
+  const [globalError, setGlobalError] = useState<string>('')
   return (
     <div className="flex items-center justify-center min-h-screen p-4">
       <Card className="w-full max-w-md">
@@ -9,6 +16,15 @@ export default function SignIn() {
             Bienvenido
           </CardTitle>
         </CardHeader>
+        <CardContent>
+          {globalError && <ErrorMessage error={globalError} />}
+          <Form>
+            <form
+              onSubmit={() => console.log('submited')}
+              className="space-y-8"
+            ></form>
+          </Form>
+        </CardContent>
       </Card>
     </div>
   )
